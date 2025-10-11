ac9b5f0d0fac459557adfba0cfc52575633b1258
andres <aarrietameneses@gmail.com>
2025-10-11 14:22:24 -0500

    se agregaron trabajos en casa

---
+++ src/arraylist/Main.java
@@ -0,0 +1,16 @@
+package arraylist;
+
+import arraylist.model.Inventory;
+import arraylist.model.Product;
+
+public class Main {
+public static void main(String[] args) {
+    Inventory inventory = new Inventory();
+    
+    inventory.addProduct(new Product("arroz", "1", 100));
+    inventory.addProduct(new Product("azucar", "2", 200));
+    inventory.addProduct(new Product("cafe", "3", 300));
+
+    inventory.searchProduct("1");
+}    
+}
